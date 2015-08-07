efbf8a1eb26292dff9845e4d38376ad535a023fd
AJ <a@ava.to>
2015-08-07 03:54:38 +0200

    layerswp first integration

--- js/app.js
+++ js/app.js
@@ -6,19 +6,18 @@ var app = {
 
 	getPosts: function() {
 
-		var rootURL = 'http://church.ava.to/wp-json';
+		var rootURL = 'http://church.ava.to';
 
 		$.ajax({
 			type: 'GET',
-			url: rootURL + '/posts?type[]=post',
+			url: rootURL + '/mobiledata/5',
 			dataType: 'json',
 			success: function(data){
 
 				console.log(data);
-				
-				$.each(data, function(index, value) {
-			      $('.main_list').append('<ons-list-item modifier="chevron" class="list-item-container"><ons-row><ons-col><div class="name">'+value.title+'</div><div class="desc">'+value.excerpt+'</div></ons-col><ons-col width="40px"></ons-col></ons-row></ons-list-item>');
-			    });
+
+			   $('.main_list').append(data.data);
+
 			},
 			error: function(error){
 				console.log(error);
